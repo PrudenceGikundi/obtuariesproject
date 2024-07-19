c94a07c8968dde40bcd03d8b14613f35797c7b2f
PrudenceGikundi <prudencekendi101@gmail.com>
2024-07-19 06:45:50 +0300

    third

---
+++ models.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Obituary(db.Model):
+    __tablename__ = 'obituary'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    date_of_birth = db.Column(db.Date, nullable=False)
+    date_of_death = db.Column(db.Date, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    submission_date = db.Column(db.DateTime, nullable=False)
+    slug = db.Column(db.String(100), unique=True)
---
+++ query_db.py
@@ -0,0 +1,16 @@
+from obtuariesproject.app import db, Obituary
+
+def view_obituaries():
+    obituaries = Obituary.query.all()
+    for obituary in obituaries:
+        print(f"ID: {obituary.id}")
+        print(f"Name: {obituary.name}")
+        print(f"Date of Birth: {obituary.date_of_birth}")
+        print(f"Date of Death: {obituary.date_of_death}")
+        print(f"Content: {obituary.content}")
+        print(f"Author: {obituary.author}")
+        print(f"Submission Date: {obituary.submission_date}")
+        print("----------")
+
+if __name__ == "__main__":
+    view_obituaries()
